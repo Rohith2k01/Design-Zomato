6b0bf272c62981a45be31a5d7d3c11420be720c7
rohith <rohith2k01@gmail.com>
2025-04-08 17:20:29 +0530

    Swiggy desing started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import './App.css'
+import { HiArrowUpRight } from "react-icons/hi2";
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      {/* First section Poster */}
+      <section className='first_section_main'>
+
+        <div className='first-navbar-section'>
+
+          <div className='first-navbar-left'>
+            <img src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png" 
+            height={'60px'} width={'200px'} alt="" />
+
+          </div>
+
+          <div className='first-navbar-right'>
+            <p className='first-navbar-right-content'>Swiggy</p>
+            <p className='first-navbar-right-content'>Partner</p>
+
+            <button className='first-navbar-get-btn'>Get the App <HiArrowUpRight className='get-btn' /></button>
+            <button className='first-navbar-signin-btn'>Sign in</button>
+
+          </div>
+
+        </div>
+
+        <div className='first-content-section'>
+
+          <div className='first-content-heading'>Order food & groceries. Discover best restaurants. Swiggy it!</div>
+
+          <div className='first-content-input-section'>
+            <select className='first-content-location'>
+              <option> Enter your delivery location</option>
+            </select>
+            <input type="text" placeholder='Search for restaurent, item or more' />
+          </div>
+
+        </div>
+
+      </section>
+    </div>
   )
 }
 

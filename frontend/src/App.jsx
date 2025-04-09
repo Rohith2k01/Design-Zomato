e31e83aebf930cc6f84d8198c97751578c9850cb
rohith <rohith2k01@gmail.com>
2025-04-09 17:11:09 +0530

    second section updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './App.css'
 import { HiArrowUpRight } from "react-icons/hi2";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
+import { CiSearch } from "react-icons/ci"
 
 function App() {
   return (
@@ -11,8 +14,8 @@ function App() {
         <div className='first-navbar-section'>
 
           <div className='first-navbar-left'>
-            <img src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png" 
-            height={'60px'} width={'200px'} alt="" />
+            <img src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png"
+              height={'60px'} width={'200px'} alt="" />
 
           </div>
 
@@ -35,8 +38,84 @@ function App() {
             <select className='first-content-location'>
               <option> Enter your delivery location</option>
             </select>
-            <input type="text" placeholder='Search for restaurent, item or more' />
+            <FaLocationDot className='location-icon-first' />
+            <IoIosArrowDown className='down-arrow-first' />
+            <input className='first-content-search' type="text" placeholder='Search for restaurent, item or more' />
+            <CiSearch className='search-icon-first' />
           </div>
+
+        </div>
+
+        {/* first-last-section */}
+        <div className='first-last-section'>
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png"
+            alt="" height={'400px'} width={'350px'} />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png"
+            alt="" height={'400px'} width={'350px'} />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png"
+            alt="" height={'400px'} width={'350px'} />
+
+        </div>
+
+      </section>
+
+      {/* second section product */}
+
+      <section className='second-section-main'>
+
+        <div className='second-inner'>
+
+          <button style={{ all: 'unset' }}>
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+              height={'150px'} width={'150px'} alt="" />
+
+          </button>
+
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+          <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Momo.png"
+            height={'150px'} width={'150px'} alt="" />
+
+
+
+
 
         </div>
 
